d767cffcab2f8056004b56f5fc5e6fe5a3df39d5
Mike <michalpso@gmail.com>
2015-03-18 23:54:26 +0000

    integrated amazonws w/ heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,11 @@ Rails.application.configure do
 
   # tell paperclip to store things in AWS
   config.paperclip_defaults={
-    storage: s3,
-    s3_credentials: {
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    :storage=>:s3,
+    :s3_credentials => {
+      :bucket=>ENV['AWS_BUCKET'],
+      :access_key_id=>ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key=>ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 
