9d905a3462642901cf2764259b3441bc08b544fa
Mike <michalpso@gmail.com>
2015-03-19 20:02:31 +0000

    fixed the views

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,9 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb =>"100x100>"}
 	# allows all image types to be uploaded
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	attr_accessor :image_file_name
+	attr_accessor :image_content_type
+	attr_accessor :image_file_size
+	attr_accessor :image_updated_at
 end
